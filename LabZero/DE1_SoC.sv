293f9321538c3c8133ae37568ce0ed48aff651a5
dneupan1 <46495785+dneupan1@users.noreply.github.com>
2023-09-20 19:22:35 -0400

    committing remaining files from lab zero,
    I'm not sure what these files are for but committing them in case they are needed by quartus for project execution

--- LabZero/DE1_SoC.sv
+++ LabZero/DE1_SoC.sv
@@ -16,3 +16,22 @@ module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
 	
 endmodule
 
+
+module DE1_SoC_testbench();
+	logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+	logic [9:0] LEDR;
+	logic [3:0] KEY;
+	logic [9:0] SW;
+	
+	DE1_SoC dut ( HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
+	
+	integer i;
+	initial begin
+		SW[9] = 1'b0;
+		SW[8] = 1'b0;
+		for (i=0; i<2**8;i++) begin
+			SW[7:0] = i;
+			#10;
+		end
+	end
+endmodule
