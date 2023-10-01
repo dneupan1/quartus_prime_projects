809a0dbf768d03673c2824b5a953c70521d36477
dneupan1 <46495785+dneupan1@users.noreply.github.com>
2023-09-30 23:31:58 -0400

    added test bench to test the addition

--- HomeWork3nAdder/HomeWork3Adder.vhd
+++ HomeWork3nAdder/HomeWork3Adder.vhd
@@ -38,3 +38,72 @@ begin
         result <= temp_result(N-1 downto 0);
     end process;
 end architecture behavior;
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all; -- For arithmetic operations
+
+entity adder_subtractor_tb is
+end entity adder_subtractor_tb;
+
+architecture sim of adder_subtractor_tb is
+    -- Parameters
+    constant N : integer := 8;
+    
+    -- Signals
+    signal add_sub : std_logic;
+    signal x, y, result : std_logic_vector(N-1 downto 0);
+    
+    -- Instantiate the unit under test (UUT)
+    component adder_subtractor is
+        generic (N : integer := 8);
+        port(
+            add_sub : in std_logic;
+            x : in std_logic_vector(N-1 downto 0);
+            y : in std_logic_vector(N-1 downto 0);
+            result : out std_logic_vector(N-1 downto 0)
+        );
+    end component;
+    
+begin
+    -- Instantiate UUT
+    UUT: adder_subtractor generic map (N => N) port map (
+        add_sub => add_sub,
+        x => x,
+        y => y,
+        result => result
+    );
+    
+    -- Testbench process
+    tb_proc: process
+    begin
+        -- Test 1: Addition
+        add_sub <= '0';
+        x <= "00000011";
+        y <= "00000001";
+        wait for 10 ns;
+        
+        -- Test 2: Subtraction
+        add_sub <= '1';
+        x <= "00000001";
+        y <= "00000001";
+        wait for 10 ns;
+        
+        -- Test 3: Another addition
+        add_sub <= '0';
+        x <= "00000011";
+        y <= "00000100";
+        wait for 10 ns;
+        
+        -- Test 4: Another subtraction
+        add_sub <= '1';
+        x <= "00000011";
+        y <= "00000001";
+        wait for 10 ns;
+        
+        -- Stop simulation
+        assert false report "End of simulation" severity failure;
+    end process tb_proc;
+
+end architecture sim;
