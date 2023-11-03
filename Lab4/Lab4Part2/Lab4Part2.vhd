bc9ea68b021250715cbd333740d65e82a90f2693
dneupan1 <46495785+dneupan1@users.noreply.github.com>
2023-11-03 18:31:28 -0400

    added placeholder files for lab4 part2
    Implementation for the lab has not been done.

---
+++ Lab4/Lab4Part2/Lab4Part2.vhd
@@ -0,0 +1,11 @@
+-- Lab4PartII 
+
+-- Students
+--		Deepak Neupane
+--		Hansen Shamoon
+--
+-- In this lab, we are to implement an alternative way to specify a counter
+-- by using a register and adding 1 to it's value. 
+-- 		Q <= Q + 1  (incrementing register value as counter)
+
+LIBRARY IEEE;
