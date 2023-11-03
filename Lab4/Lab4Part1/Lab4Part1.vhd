81807f64a7cd4ff28bd8064c1df92baa2b1a5dcb
dneupan1 <46495785+dneupan1@users.noreply.github.com>
2023-11-03 18:25:06 -0400

    added the vhdl file for lab4 part I

---
+++ Lab4/Lab4Part1/Lab4Part1.vhd
@@ -0,0 +1,12 @@
+-- LabFourPartI
+--
+-- Students:
+-- 	Deepak Neupane
+--		Hansen Shamoon
+--
+-- In this part, we need to implement a 8 bit counter using 8 instances of 
+-- T-Type flip flop
+
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+
