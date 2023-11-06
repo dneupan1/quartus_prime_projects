7335ed7ffd562fdcdeb24e2b00cc69c1ad28108c
dneupan1 <46495785+dneupan1@users.noreply.github.com>
2023-11-06 16:09:53 -0500

    LabSix

---
+++ LabSix/src/HelloWorld.c
@@ -0,0 +1,15 @@
+/*
+ * HelloWorld.c
+ *
+ *  Created on: Nov 6, 2023
+ *      Author: Deepak
+ */
+
+
+#include<stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World");
+	return 0;
+}
