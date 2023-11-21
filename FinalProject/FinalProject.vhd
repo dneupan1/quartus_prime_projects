4a51d3fa4d3190cffb406aa54f6986b842ef7736
dneupan1 <46495785+dneupan1@users.noreply.github.com>
2023-11-20 19:43:01 -0500

    skeleton for final project

---
+++ FinalProject/FinalProject.vhd
@@ -0,0 +1,28 @@
+-- Final Project
+--
+-- Students:
+--	 Hansen Shamoon 
+--	 Deepak Neupane
+--  Digvijay Jonna
+--
+--
+
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+-- Four Bit Adder (Top Level Entity)
+ENTITY FinalProject is
+    PORT ( 
+	 
+	 );
+END FinalProject;
+
+ARCHITECTURE Behavioral of FinalProject is
+
+
+BEGIN
+	
+    
+END Behavioral;
+
